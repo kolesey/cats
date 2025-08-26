897d1ca7fd1701d5bea3e2f14ff2ef4de6a2638b
Garik <igor@koleso40.ru>
2025-08-26 21:30:46 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,88 @@
+import sys
+from classes import Cats
+from classes import YaDisk
+import requests
+import json
+
+FOLDER = 'PD-137'
+text = input('Введите текст: ')
+
+# Создаем кота с подписью
+try:
+    cat = Cats(text)
+    print ('Создали кота.')
+except requests.ConnectionError:
+    print(f'Нет связи с сервером cataas.com')
+    sys.exit(0)
+except requests.Timeout:
+    print(f'Превышено время ожидания ответа от cataas.com')
+    sys.exit(0)
+
+# Получаем путь к изображению
+cat_url = cat.get_cat_url()
+
+token = input('Введите токен Яндекс Диска: ')
+disk = YaDisk(token)
+
+# Проверяем, есть ли папка на диске, если нет, создаем ее
+try:
+    if disk.create_folder(FOLDER) == False:
+        print('Ошибка при создании папки на Яндекс Диске.')
+        sys.exit()
+    else:
+        print('Папка успешно создана на Яндекс Диске.')
+except requests.ConnectionError:
+    print('Нет связи с Яндекс Диском')
+    sys.exit()
+except requests.Timeout:
+    print(f'Превышено время ожидания ответа от Яндекс Диска')
+    sys.exit()
+
+# Формируем json c размером файла
+data = {
+    'filesize': cat.get_cat_size()
+}
+
+# Загружаем json на диск, проверяя не существует ли такой файл уже на диске
+try:
+    resp = disk.upload_file(f'/{FOLDER}/{text}.json', json.dumps(data))
+    if resp == True:
+        print('json загружен на Яндекс Диск')
+    elif resp == 'FileExist':
+        print('Кот с таким текстом уже существует.')
+        sys.exit()
+    else:
+        print('Неизвестная ошибка при загрузке json файла на диск')
+        sys.exit()
+except requests.ConnectionError:
+    print('Нет связи с Яндекс Диском')
+    sys.exit()
+except requests.Timeout:
+    print(f'Превышено время ожидания ответа от Яндекс Диска')
+    sys.exit()
+
+
+# Формируем путь к файлу
+filename = f'/{FOLDER}/{text}.{cat.get_cat_extension()}'
+
+# Загружаем изображение кота с текстом на диск
+try:
+    if disk.upload_file_by_url(filename, cat_url) == False:
+        print('Ошибка при загрузке кота на Яндекс Диск.')
+        sys.exit()
+    else:
+        print('Кот успешно загружен на Яндекс Диск')
+except requests.ConnectionError:
+    print('Нет связи с Яндекс Диском')
+    sys.exit()
+except requests.Timeout:
+    print(f'Превышено время ожидания ответа от Яндекс Диска')
+    sys.exit()
+
+
+
+
+
+
+
+
---
+++ classes.py
@@ -0,0 +1,154 @@
+import requests
+
+class Cats:
+    CAT_URL = 'https://cataas.com'
+
+    def __init__(self, text):
+        self.text = text
+        self.json = self._get_image_json()
+
+    def get_cat_id(self):
+        """
+        Метод возвращает ID созданного изображения
+        :return: ID созданного изображения
+        """
+        return self.json['id']
+
+    def get_cat_extension(self):
+        """
+        Метод возвращает расширение созданного изображения
+        :return: расширение созданного изображения
+        """
+        mimetype = self.json['mimetype']
+        return mimetype.split('/')[-1]
+
+    def get_cat_size(self):
+        """
+        Метод получает размер созданного изображения
+        :return: размер созданного изображения в байтах
+        """
+        response = requests.get(self.get_cat_url())
+        return response.headers['Content-Length']
+
+    def get_cat_url(self):
+        """
+        Метод возвращает url созданного изображения
+        :return: url созданного изображения
+        """
+        return f'{self.CAT_URL}/cat/{self.get_cat_id()}/says/{self.text}'
+        # return self.json['url']
+
+
+    def _get_image_json(self):
+        url = f'{self.CAT_URL}/cat/says/{self.text}'
+        params = {
+            'json': 'true'
+        }
+
+        response = requests.get(url, params=params)
+        return response.json()
+
+
+
+class YaDisk:
+    YD_URL = 'https://cloud-api.yandex.net/v1/disk/resources'
+
+    def __init__(self, token):
+        self.token = token
+        self.headers = {
+            'Authorization': 'OAuth ' + self.token
+        }
+
+    def check_folder(self, folder_name):
+        """
+        Метод проверяет наличие папки на Яндекс диске
+        :param folder_name: имя папки
+        :return: True если папка есть. False если папки нет
+        """
+
+        params = {
+            'path': folder_name
+        }
+
+        response = requests.get(self.YD_URL, params=params, headers=self.headers)
+
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+
+
+    def create_folder(self, folder_name):
+        """
+        Метод создает папку на яндекс диске
+        :param folder_name: Имя папки
+        :return: True если папка создана или уже существовала на диске. False если возникли проблемы
+        """
+
+        params = {
+            'path': folder_name
+        }
+
+        # Если такой папки не существует, то создаем ее
+        if self.check_folder(folder_name) == False:
+            response = requests.put(self.YD_URL, params=params, headers=self.headers)
+            if response.status_code == 201:
+                return True
+            else:
+                return False
+        # Если папка существует, то ничего не делаем
+        else:
+            return True
+
+    def upload_file_by_url(self, filename, file_url):
+        """
+        Метод загружает файл на диск по переданному url изображения
+        :param filename: путь и имя файла который загружается на диск
+        :param file_url: url изображения
+        :return: True если файл загружен.
+        """
+        url = self.YD_URL + '/upload'
+        params = {
+            'path': filename,
+            'url': file_url
+        }
+        response = requests.post(url, params=params, headers=self.headers)
+        if response.status_code == 202:
+            return True
+        else:
+            return False
+
+    def get_upload_url(self, path):
+        """
+        Метод создает ссылку для загрузки файла на диск
+        :param path: путь с именем файла на диске
+        :return: json ответ сервера
+        """
+        url = self.YD_URL + '/upload'
+        params = {
+            'path': path,
+
+        }
+        response = requests.get(url, params=params, headers=self.headers)
+
+        return response.json()
+
+    def upload_file(self, path, file):
+        """
+        Метод загружает файл на диск
+        :param path: путь с именем файла на диске
+        :param file: файл для загрузки
+        :return: 'FileExist' если файл уже существует. True если файл загрузился
+        """
+        upload_url_dict = self.get_upload_url(path)
+        if upload_url_dict.get('error') == 'DiskResourceAlreadyExistsError':
+            return 'FileExist'
+        elif upload_url_dict.get('href', '') == '':
+            return False
+        else:
+            upload_url = upload_url_dict.get('href')
+        response = requests.put(upload_url, files={'file': file})
+        if response.status_code == 201:
+            return True
+        else:
+            return False
